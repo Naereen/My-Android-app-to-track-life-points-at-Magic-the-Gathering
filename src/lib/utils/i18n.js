726146ade9baa1e6aaeb5abbf8d4c65883706347
Lilian Besson <lilian.besson@crans.org>
2026-01-25 15:10:49 +0100

    TRYING, it's exhausting.

--- src/lib/utils/i18n.js
+++ src/lib/utils/i18n.js
@@ -1,8 +1,8 @@
 import { register, init, getLocaleFromNavigator } from 'svelte-i18n';
 
 // On lie les fichiers JSON de traduction
-await register('en', () => import('../../locales/en.json'));
-await register('fr', () => import('../../locales/fr.json'));
+register('en', () => import('../../locales/en.json'));
+register('fr', () => import('../../locales/fr.json'));
 
 init({
     fallbackLocale: 'fr', // Langue par défaut si la détection échoue
