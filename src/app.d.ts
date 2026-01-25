0c5cbf18cac1866ab670bd0d667c38870bddf97a
Lilian Besson <lilian.besson@crans.org>
2026-01-25 15:20:33 +0100

    Apparently it should finally work.

--- src/app.d.ts
+++ src/app.d.ts
@@ -25,5 +25,3 @@ declare global {
 }
 
 export {};
-
-import 'lib/utils/i18n.js'; // Importation pour initialiser i18n
--- src/hooks.client.js
+++ src/hooks.client.js
@@ -1,9 +1,9 @@
 import { register, init, getLocaleFromNavigator } from 'svelte-i18n';
 
 // On lie les fichiers JSON de traduction
-register('en', () => import('../../locales/en.json'));
-register('fr', () => import('../../locales/fr.json'));
-register('en', () => import('../../locales/es.json'));
+register('en', () => import('./locales/en.json'));
+register('fr', () => import('./locales/fr.json'));
+register('en', () => import('./locales/es.json'));
 
 init({
     fallbackLocale: 'fr', // Langue par défaut si la détection échoue
