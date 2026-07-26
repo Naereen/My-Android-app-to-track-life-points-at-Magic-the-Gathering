ca278621dc2cb97fddabb12e14ae4062a67239d0
Lilian Besson <lilian.besson@crans.org>
2026-07-26 21:26:36 +0200

    :bug: Trying to work on #40 ...

--- src/lib/components/player/PlayerHorizontal.svelte
+++ src/lib/components/player/PlayerHorizontal.svelte
@@ -323,6 +323,7 @@
 		class="relative z-20 flex w-full rounded-2xl flex-grow h-6"
 		class:h-full={!$appState.isMenuOpen}
 		class:opacity-75={$players[index].highlighted}
+		class:bg-player-dark={isDead}
 	>
 		{#if !$appState.isMenuOpen}
 			<div class="flex flex-col w-full relative">
--- src/lib/components/player/PlayerVertical.svelte
+++ src/lib/components/player/PlayerVertical.svelte
@@ -289,6 +289,7 @@
 		class="relative z-20 flex w-full rounded-2xl flex-grow h-6"
 		class:h-full={!$appState.isMenuOpen}
 		class:opacity-75={$players[index].highlighted}
+		class:bg-player-dark={isDead}
 	>
 		{#if !$appState.isMenuOpen}
 			<div class="flex w-full relative">
